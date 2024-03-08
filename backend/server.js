ac5e05b6e77219dbb4ef3b2806794ba665140bb4
Ammaar-Alam <ammaar@princeton.edu>
2024-03-08 17:05:31 -0500

    reverting to prior working verision without css

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// serve static files from the directory where index.html is located
+// Serve static files from the directory where index.html is located
 app.use(express.static(path.join(__dirname, "..")));
 
 io.on("connection", (socket) => {
@@ -16,7 +16,7 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", () => {
     if (addedUser) {
-      // broadcast a user left notification
+      // Broadcast a user left notification
       socket.broadcast.emit("user left", socket.username);
     }
     console.log("Client disconnected");
@@ -25,40 +25,20 @@ io.on("connection", (socket) => {
   socket.on("add user", (username) => {
     if (addedUser) return;
 
-    // store the username in the socket session
+    // Store the username in the socket session
     socket.username = username;
     addedUser = true;
     socket.broadcast.emit("user joined", { username: socket.username });
   });
 
   socket.on("sendMessage", (data) => {
-    // broadcast message to all clients
+    // Broadcast message to all clients
     io.emit("message", {
       username: socket.username || "Anonymous",
       message: data.message,
     });
-
-    socket.on("user joined", (data) => {
-      // Update user list
-      updateUserList(data.users);
-    });
-
-    socket.on("user left", (data) => {
-      // Update user list
-      updateUserList(data.users);
-    });
-
-    function updateUserList(users) {
-      const userListElement = document.getElementById("userList");
-      userListElement.innerHTML = ""; // Clear current list
-      users.forEach((user) => {
-        const userElement = document.createElement("li");
-        userElement.textContent = user;
-        userListElement.appendChild(userElement);
-      });
-    }
   });
 });
 
-const PORT = process.env.PORT || 3120;
+const PORT = process.env.PORT || 54321;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
