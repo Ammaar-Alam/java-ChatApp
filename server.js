3f1ab8779e4f56226cb720b543e5017c5d72eb64
Ammaar-Alam <ammaar@princeton.edu>
2024-03-08 15:26:12 -0500

    websocket finally works
    the html looks really bad but it officially works yayya

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+// Serve static files from the directory where index.html is located
+app.use(express.static(path.join(__dirname)));
 
 io.on("connection", (socket) => {
   console.log("New client connected");
